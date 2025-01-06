93749c2c972309d864eea3e0cccc99bcffcaa825
Mr_Ehtsham <mr_ehtsham@yahoo.com>
2025-01-06 00:10:02 -0800

    Updated code

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.api import router
+import uvicorn
+
+# Set TensorFlow environment variables to handle warnings
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  # Disable oneDNN custom operations
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'   # Reduce TensorFlow logging verbosity
+
 app = FastAPI()
 
+# Define allowed origins
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+]
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
+    expose_headers=["Content-Type"]
+)
+
+# Include router
 app.include_router(router=router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
